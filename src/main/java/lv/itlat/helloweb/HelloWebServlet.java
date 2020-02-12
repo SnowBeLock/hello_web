a1a7d6db7e8bc771c5f17987a92cfb2c2ee87c6a
SnowBeLock <karkardro@gmail.com>
2020-02-12 20:53:14 +0200

    Initial commit

---
+++ src/main/java/lv/itlat/helloweb/HelloWebServlet.java
@@ -0,0 +1,20 @@
+package lv.itlat.helloweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "helloWebServlet", urlPatterns = {"/hello"}, loadOnStartup = 1)
+public class HelloWebServlet extends HttpServlet {
+
+    @Override
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello from WEB Servlet");
+    }
+}

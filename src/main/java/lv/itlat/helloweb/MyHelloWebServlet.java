c99168cbc9d0c4ed7b5082c0bce7eda286e19660
SnowBeLock <karkardro@gmail.com>
2020-02-19 19:05:12 +0200

    no comm

--- src/main/java/lv/itlat/helloweb/MyHelloWebServlet.java
+++ src/main/java/lv/itlat/helloweb/MyHelloWebServlet.java
@@ -1,4 +1,5 @@
 package lv.itlat.helloweb;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,10 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "myHelloWebServlet", urlPatterns = {"/myhello"},loadOnStartup = 1)
-public class MyHelloWebServlet extends HttpServlet{
+@WebServlet(name = "myHelloWebServlet", urlPatterns = {"/myhello"}, loadOnStartup = 1)
+public class MyHelloWebServlet extends HttpServlet {
     @Override
-    protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<h1>MY TRUE OWN SERVLET</h1>");
@@ -18,12 +19,19 @@ public class MyHelloWebServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstName=req.getParameter("firstName");
-        String surname=req.getParameter("surname");
-        String email=req.getParameter("email");
-        PrintWriter printWriter= resp.getWriter();
+        String firstName = req.getParameter("firstName");
+        String surname = req.getParameter("surname");
+        String email = req.getParameter("email");
+        String gender = req.getParameter("gender");
+        String comments = req.getParameter("comment");
+        PrintWriter printWriter = resp.getWriter();
 
-        printWriter.println("<h1>"+firstName+" " +  surname+ " logged in" +"</h1>");
+        printWriter.println("<h1>" + firstName + " " + surname + " logged in" + "</h1>");
+
+
+        if (firstName == null ||  firstName.isEmpty()) {
+            resp.sendError(resp.SC_BAD_REQUEST, "YOUR FIRS NAME IS EMPTY. WE NEEED TOUR NAME!!!");
+        }
         printWriter.close();
     }
 }

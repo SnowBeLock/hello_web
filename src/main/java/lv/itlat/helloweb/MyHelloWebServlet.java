69f72eaba4a83adbdc042e65ad21734b9d0cf8eb
SnowBeLock <karkardro@gmail.com>
2020-02-19 20:32:29 +0200

    Logger

--- src/main/java/lv/itlat/helloweb/MyHelloWebServlet.java
+++ src/main/java/lv/itlat/helloweb/MyHelloWebServlet.java
@@ -1,5 +1,7 @@
 package lv.itlat.helloweb;
 
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 @WebServlet(name = "myHelloWebServlet", urlPatterns = {"/myhello"}, loadOnStartup = 1)
 public class MyHelloWebServlet extends HttpServlet {
+
+
+    private Logger logger= Logger.getLogger(MyHelloWebServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<h1>MY TRUE OWN SERVLET</h1>");
@@ -19,6 +26,7 @@ public class MyHelloWebServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Servlet post method started");
         String firstName = req.getParameter("firstName");
         String surname = req.getParameter("surname");
         String email = req.getParameter("email");
@@ -38,5 +46,6 @@ public class MyHelloWebServlet extends HttpServlet {
         }
 
         printWriter.close();
+        logger.info("Servlet finished");
     }
 }
